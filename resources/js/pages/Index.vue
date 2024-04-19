d21a3f8bee7c435cc4f9ff48ffa283eb54ac8af3
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2024-04-19 09:35:34 +0300

    adding ads script

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -54,6 +54,18 @@
                 </div>
         </div><!--end container-->
 
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6249647619521156"
+                                crossorigin="anonymous"></script>
+                        <ins class="adsbygoogle"
+                             style="display:block; text-align:center;"
+                             data-ad-layout="in-article"
+                             data-ad-format="fluid"
+                             data-ad-client="ca-pub-6249647619521156"
+                             data-ad-slot="7021661895"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+
                     </div>
                   </Transition>
 
